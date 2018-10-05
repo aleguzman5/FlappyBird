d4d6e714ef830b6aca856e7c4633de5f1a3c5b79
aleguzman5 <shmc555@gmail.com>
2018-10-05 11:35:04 -0500

    Updating author comment

--- Flappy.java
+++ Flappy.java
@@ -3,8 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Flappy here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Alejandro Guzman 
  */
 public class Flappy extends Actor
 {
